9be1e66e50f48c2fa3a6d6ab9b074a1034e34ddd
James Walker <james.chunho@gmail.com>
2024-03-16 21:25:34 +0800

    resource directive

--- src/parser.rs
+++ src/parser.rs
@@ -3,8 +3,8 @@ use std::{path::PathBuf, str::FromStr};
 use nom::{
     bytes::complete::{escaped, tag, take, take_till, take_till1},
     character::complete::{alpha1, char, space0, space1},
-    combinator::recognize,
-    sequence::{delimited, preceded, Tuple},
+    combinator::{opt, recognize},
+    sequence::{delimited, preceded, terminated, tuple, Tuple},
     Err, IResult,
 };
 use nom_locate::LocatedSpan;
@@ -19,6 +19,8 @@ enum ParseError<I> {
     MalformedPath(I),
     #[error("only relative paths are allowed `{0}`")]
     NotRelativePath(I),
+    #[error("invalid glob pattern `{0}`")]
+    InvalidGlobPattern(I),
     #[error("invalid syntax: {0:?}")]
     Nom(I, nom::error::ErrorKind),
 }
@@ -57,21 +59,57 @@ fn line_comment(input: Input) -> Result {
 #[derive(Debug)]
 enum Directive<'a> {
     Include(RelativePathBuf),
+    Resource {
+        pattern: glob::Pattern,
+        dest: RelativePathBuf,
+    },
     Unknown {
         name: Input<'a>,
         contents: Input<'a>,
     },
 }
 
-fn include_directive(input: Input) -> Result<Directive> {
-    let (rest, (_, _, path, _)) = (tag("#include"), space1, string, space0).parse(input)?;
+fn relative_path_string(input: Input) -> Result<RelativePathBuf> {
+    let (rest, raw_string) = string(input)?;
 
-    let path =
-        PathBuf::from_str(path.as_str()).or(Err(Err::Failure(ParseError::MalformedPath(input))))?;
+    // convert to PathBuf, may be absolute
+    let path = PathBuf::from_str(raw_string.as_str())
+        .or(Err(Err::Failure(ParseError::MalformedPath(input))))?;
+    // convert to RelativePathBuf, must be relative now
     let path = RelativePathBuf::from_path(path)
         .or(Err(Err::Failure(ParseError::NotRelativePath(input))))?;
 
+    Ok((rest, path))
+}
+
+fn include_directive(input: Input) -> Result<Directive> {
+    let (rest, (_, _, path, _)) =
+        (tag("#include"), space1, relative_path_string, space0).parse(input)?;
+
     Ok((rest, Directive::Include(path)))
+}
+
+fn resource_directive(input: Input) -> Result<Directive> {
+    let (rest, (_, _, dest, pattern, _)) = (
+        tag("#resource"),
+        space1,
+        opt(terminated(
+            relative_path_string,
+            tuple((space0, char(':'), space0)),
+        )),
+        relative_path_string,
+        space0,
+    )
+        .parse(input)?;
+
+    // default destination to "."
+    let dest = dest.unwrap_or(RelativePathBuf::from("."));
+
+    // parse pattern
+    let pattern = glob::Pattern::new(pattern.as_str())
+        .or(Err(Err::Failure(ParseError::InvalidGlobPattern(input))))?;
+
+    Ok((rest, Directive::Resource { pattern, dest }))
 }
 
 fn unknown_directive(input: Input) -> Result<Directive> {
@@ -96,7 +134,7 @@ mod tests {
         dbg!(&result);
 
         let result = result.finish();
-        assert!(result.is_ok(), "{:?}", result);
+        assert!(result.is_ok(), "should be ok: {:?}", result);
 
         let result = result.unwrap();
         assert_eq!(
@@ -115,7 +153,12 @@ mod tests {
         dbg!(&result);
 
         let result = result.finish();
-        assert!(result.is_err(), "{:?}", result);
+        match result {
+            Ok(result) => {
+                assert!(result.0.len() > 0, "should be err: {:?}", result);
+            }
+            Err(_) => (),
+        }
     }
 
     #[test]
@@ -150,5 +193,13 @@ mod tests {
         ok(unknown_directive(
             r#"#include    "C:/test/tcp.rtconfig.txt"  "#.into(),
         ));
+
+        ok(resource_directive(r#"#resource "./*.png""#.into()));
+        ok(resource_directive(r#"#resource    "./*.png"  "#.into()));
+        ok(resource_directive(r#"#resource "./knob.png""#.into()));
+        ok(resource_directive(r#"#resource "150": "./*.png""#.into()));
+        ok(resource_directive(r#"#resource "150" : "./*.png""#.into()));
+        bad(resource_directive(r#"#resource "150" "./*.png""#.into()));
+        bad(resource_directive(r#"#resource "C:/knob.png""#.into()));
     }
 }
